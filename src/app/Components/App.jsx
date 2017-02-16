2e7aeb9c97898c6412c7d6649b0dcf1c273409c8
Nedjim DANIMON NGABA <nedjim.danimon@gmail.com>
2017-02-16 10:58:04 +0100

    Add init style

--- src/app/Components/App.jsx
+++ src/app/Components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header/Header.jsx';
+import Content from './Content/Content.jsx';
 
-export default class Content extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
             <div>
-                Coucou
+                <Header />
+                <Content />
             </div>
         )
     }
---
+++ src/app/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+        return (
+            <header>
+               <h1>Vélib / Parking</h1>
+            </header>
+        )
+}
+
+export default Header;
