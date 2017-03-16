42ea68aacf30d25377e813a49033b66bfb58e2f5
Nedjim <nedjim.danimon@gmail.com>
2017-03-16 16:54:35 +0100

    init heroku

--- src/app/Components/App.jsx
+++ src/app/Components/App.jsx
@@ -2,7 +2,7 @@ import React            from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 import Velib            from './Content/Velib/Content.jsx';
 import Parking          from './Content/Parking/Content.jsx';
-import Home          from './Home.jsx';
+import Home             from './Home.jsx';
 
 export default class App extends React.Component {
     render() {
